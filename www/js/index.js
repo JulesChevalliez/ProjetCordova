221b20abad835b8b5c659bcdd2a4c2496cbdb7fa
JulesChevalliez <56346843+JulesChevalliez@users.noreply.github.com>
2020-01-23 10:31:58 +0100

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,7 @@
 let lieux = "";
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -28,99 +28,164 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
-    
+
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        
+    receivedEvent: function (id) {
+
         function distance(lat1, lon1, lat2, lon2, unit) {
             if ((lat1 == lat2) && (lon1 == lon2)) {
                 return 0;
             }
             else {
-                var radlat1 = Math.PI * lat1/180;
-                var radlat2 = Math.PI * lat2/180;
-                var theta = lon1-lon2;
-                var radtheta = Math.PI * theta/180;
+                var radlat1 = Math.PI * lat1 / 180;
+                var radlat2 = Math.PI * lat2 / 180;
+                var theta = lon1 - lon2;
+                var radtheta = Math.PI * theta / 180;
                 var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
                 if (dist > 1) {
                     dist = 1;
                 }
                 dist = Math.acos(dist);
-                dist = dist * 180/Math.PI;
+                dist = dist * 180 / Math.PI;
                 dist = dist * 60 * 1.1515;
-                if (unit=="K") { dist = dist * 1.609344 }
-                if (unit=="N") { dist = dist * 0.8684 }
+                if (unit == "K") { dist = dist * 1.609344 }
+                if (unit == "N") { dist = dist * 0.8684 }
                 return dist;
             }
         }
-        
+
         $.ajax({
             url: "https://devweb.iutmetz.univ-lorraine.fr/~gonzal124u/LieuxMetz/php/getAll.php",
             type: "GET",
             success: function (result) {
                 lieux = JSON.parse(result);
+                for (let i = 0; i < lieux.length; i++) {
+                    latLieu = lieux[i]["latitude"];
+                    lngLieu = lieux[i]["longitude"];
+                    L.marker([latLieu, lngLieu]).addTo(mymap).on('click', function (e) {
+                        openInfo(lieux[i]);
+                    });
+                };
             },
             error: function (error) {
                 console.log(error);
             }
         });
-        
-        var onSuccess = function(position) {
-            console.log('Latitude: '          + position.coords.latitude          + '\n' +
-            'Longitude: '         + position.coords.longitude         + '\n' +
-            'Altitude: '          + position.coords.altitude          + '\n' +
-            'Accuracy: '          + position.coords.accuracy          + '\n' +
-            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-            'Heading: '           + position.coords.heading           + '\n' +
-            'Speed: '             + position.coords.speed             + '\n' +
-            'Timestamp: '         + position.timestamp                + '\n');
+
+        closeInfoVar = false;
+        markerLoaded = false;
+        oldLocationUser = [];
+        $('#cardInfo').hide();
+
+        function openInfo(lieux) {
+            $('.card-content').append('<h5 class="crad-title">' + lieux['nom'] + '</h5>');
+            $('.card-content').append('<div class="scrollable"><p class="card-text card-desc">' + lieux['description'] + '</p></div>');
+
+            for (u = 0; u < 3; u++) {
+                $('.card-img').append('<div class="carousel-item"><img src="https://devweb.iutmetz.univ-lorraine.fr/~chevall49u/LieuxMetz/' + lieux['visuel'] + '_' + u + '.jpg"><div class="carousel-caption"></div>   </div>');
+                $('.carousel-indicators').append('<li data-target="#carouselExampleIndicators" data-slide-to="' + u + '"></li>');
+            }
+
+            $('.carousel-item').first().addClass('active');
+            $('.carousel-indicators > li').first().addClass('active');
+            $('#carousel-example-generic').carousel();
+            $('#cardInfo').show();
+        }
+
+        var onSuccess = function (position) {
+            console.log('Latitude: ' + position.coords.latitude + '\n' +
+                'Longitude: ' + position.coords.longitude + '\n' +
+                'Altitude: ' + position.coords.altitude + '\n' +
+                'Accuracy: ' + position.coords.accuracy + '\n' +
+                'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+                'Heading: ' + position.coords.heading + '\n' +
+                'Speed: ' + position.coords.speed + '\n' +
+                'Timestamp: ' + position.timestamp + '\n');
 
             latUser = position.coords.latitude;
             lngUser = position.coords.longitude;
 
-            
+
             //navigator.vibrate([500, 100, 500, 100, 500, 100, 750]);
 
-            for( let i = 0; i<lieux.length; i++){
+            if (typeof oldLocationUser !== 'undefined' && oldLocationUser.length > 0) {
+                distOldUser = (distance(oldLocationUser[0], oldLocationUser[1], latUser, lngUser, "K") * 1000).toFixed(0)
+                console.log("Dist Old : "+distOldUser);
+                if(distOldUser > 100){
+                    closeInfoVar = false;
+                }
+            }
+
+            for (let i = 0; i < lieux.length; i++) {
+
                 latLieu = lieux[i]["latitude"];
                 lngLieu = lieux[i]["longitude"];
-                dist = (distance(latLieu,lngLieu, latUser, lngUser, "K")*1000).toFixed(0);
-                console.log(dist);
-                $('#test').append("<p>lat: "+latUser+" / lng : "+lngUser+"</p>");
-                
-                if (dist < 10){
-                    $('#exampleModal').modal('toggle');
+                dist = (distance(latLieu, lngLieu, latUser, lngUser, "K") * 1000).toFixed(0);
+                markerLoaded = true;
+
+
+                if (dist < 100) {
+
+                    L.map('mapid').setZoom(5);
+
+                    if (!closeInfoVar) {
+                        console.log(closeInfoVar);
+                        $('.card-content').append('<h5 class="crad-title">' + lieux[i]['nom'] + '</h5>');
+                        $('.card-content').append('<div class="scrollable"><p class="card-text card-desc">' + lieux[i]['description'] + '</p></div>');
+
+                        for (u = 0; u < 3; u++) {
+                            $('.card-img').append('<div class="carousel-item"><img src="https://devweb.iutmetz.univ-lorraine.fr/~chevall49u/LieuxMetz/' + lieux[i]['visuel'] + '_' + u + '.jpg"><div class="carousel-caption"></div>   </div>');
+                            $('.carousel-indicators').append('<li data-target="#carouselExampleIndicators" data-slide-to="' + u + '"></li>');
+                        }
+
+                        $('.carousel-item').first().addClass('active');
+                        $('.carousel-indicators > li').first().addClass('active');
+                        $('#carousel-example-generic').carousel();
+                        $('#cardInfo').show();
+                        closeInfoVar = true;
+                    }
                 }
+                oldLocationUser = [latUser, lngUser];
             }
 
         };
 
         var mymap = L.map('mapid').setView([49.1191, 6.1727], 13);
-            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: 'mapbox/outdoors-v11',
-                accessToken: 'pk.eyJ1IjoiZHJjb21ibyIsImEiOiJjazViMHVpY2Ywc2QyM2ZwazdtNW9xNmZjIn0.s0IQl94VBUyolsiDA1LZTg'
-            }).addTo(mymap);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/outdoors-v11',
+            accessToken: 'pk.eyJ1IjoiZHJjb21ibyIsImEiOiJjazViMHVpY2Ywc2QyM2ZwazdtNW9xNmZjIn0.s0IQl94VBUyolsiDA1LZTg'
+        }).addTo(mymap);
+
+
+
 
         function onError(error) {
-            alert('code: '    + error.code    + '\n' +
-            'message: ' + error.message + '\n');
+            alert('code: ' + error.code + '\n' +
+                'message: ' + error.message + '\n');
         }
 
-        
+
+
+        $("#btnCloseInfo").click(function () {
+            $('.card-content').empty();
+            $('.card-img').empty();
+            $('.carousel-indicators').empty();
+            $('#cardInfo').hide();
+        });
 
         var intervalID = window.setInterval(refresh, 5000);
 
-        function refresh(){
+        function refresh() {
             navigator.geolocation.getCurrentPosition(onSuccess, onError);
         }
 
 
-        
+
     }
 };
 
